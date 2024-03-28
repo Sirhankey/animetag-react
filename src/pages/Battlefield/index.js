4e31073d275bae448be50c94fa2de33fe0a01b66
Sirhankey <36062676+Sirhankey@users.noreply.github.com>
2024-03-28 16:53:30 -0300

    feat: adiciona funcionalidades battlefield

--- src/pages/Battlefield/index.js
+++ src/pages/Battlefield/index.js
@@ -1,12 +1,107 @@
+import { useFavoritosContext } from "contextos/Favoritos";
 import styles from "./Battlefield.module.css";
+import { useState } from "react";
+
+import pokeball1 from "./pokeball_1.png";
+import pokeball2 from "./pokeball_2.png";
+import pokeball3 from "./pokeball_3.png";
+
+
 
 function Battlefield() {
+    const [selectedPokemon, setselectedPokemon] = useState(null);
+    const { favorito } = useFavoritosContext();
 
+
+    const playerPkmns = favorito.slice(0, 4);
+
+    function escolherPokemon(id) {
+        if (!id) return
+        setselectedPokemon(playerPkmns.find((pokemon) => pokemon.id === id));
+        console.log('Escolhendo pokemon', selectedPokemon);
+        // console.log(selectedPokemon.sprites.other.showdown.front_default)
+    }
+
+    console.log(playerPkmns)
     return (
-        <div className={styles.container}>
-            <h1>Battlefield</h1>
-            <p>Em breve...</p>
-        </div>
+        <section className={styles.container}>
+            <div className={styles.enemy_side}>
+                <div className={styles.r_enemy_side}>
+
+                </div>
+                <div className={styles.l_enemy_side}>
+
+                </div>
+            </div>
+            <div className={styles.player_side}>
+                <div className={styles.l_player_side}>
+                    <div className={styles.player_pokemon}>
+                        {selectedPokemon && (
+                            <div className={styles.player_pokemon}>
+                                <span className={styles.pokemon_name}>
+                                    {selectedPokemon.name}
+                                </span>
+                                <img
+                                    className={styles.pokemon_img}
+                                    src={selectedPokemon.sprites.other.showdown.front_default} alt={selectedPokemon.name} />
+                                <div className={styles.pokemon_stats}>
+                                    <div className={styles.button_wrapper}>
+                                        <div className={styles.atck_button}>
+                                            <img
+                                                onClick={() => console.log('Ataque 1')}
+                                                className={styles.atck_img}
+                                                src={pokeball1}
+                                                alt="Ataque 1"
+                                            />
+
+                                        </div>
+                                        <span>Height: {selectedPokemon.heightInMeters}</span>
+                                    </div>
+
+                                    <div className={styles.button_wrapper}>
+                                        <div className={styles.atck_button}>
+                                            <img
+                                                onClick={() => console.log('Ataque 2')}
+                                                className={styles.atck_img}
+                                                src={pokeball2}
+                                                alt="Ataque 2"
+                                            />
+                                        </div>
+                                        <span>Weight: {selectedPokemon.weightInKilograms}</span>
+                                    </div>
+
+                                    <div className={styles.button_wrapper}>
+                                        <div className={styles.atck_button}>
+                                            <img
+                                                onClick={() => console.log('Ataque 3')}
+                                                className={styles.atck_img}
+                                                src={pokeball3}
+                                                alt="Ataque 3"
+                                            />
+                                        </div>
+                                        <span>Type: {selectedPokemon.types[0]}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <div className={styles.r_player_side}>
+                    <div className={styles.player_pkmns}>
+                        {playerPkmns.map((pokemon) => (
+                            <div className={styles.player_pkmn}>
+                                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                                <button
+                                    onClick={() =>
+                                        escolherPokemon(pokemon.id)
+                                    }
+                                >{pokemon.name}</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
